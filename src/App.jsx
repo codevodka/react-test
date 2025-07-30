d5d79d82900b5b0c43f401ce97067955460541bb
Sujol Ghosh <76590380+codevodka@users.noreply.github.com>
2025-07-30 10:37:14 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
-import { updateUserPlaces } from './http.js';
+import { fetchUserPlaces, updateUserPlaces } from './http.js';
+import Error from './components/Error.jsx';
 
 function App() {
   const selectedPlace = useRef();
@@ -13,6 +14,28 @@ function App() {
   const [userPlaces, setUserPlaces] = useState([]);
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState()
+
+  const [isFetching, setIsFetching] = useState(false)
+  const [error, setError] = useState()
+
+  useEffect(()=>{
+    async function fetchplaces(){
+      setIsFetching(true)
+      try{
+        const places = await fetchUserPlaces()
+        setUserPlaces(places)
+      } catch (error){
+        setError({
+          message:error.message || "Failed to fetch user places"
+        })
+      }
+      setIsFetching(false)
+
+    }
+
+    fetchplaces()
+  },[])
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -24,6 +47,8 @@ function App() {
   }
 
   async function handleSelectPlace(selectedPlace) {
+    //  await updateUserPlaces([selectedPlace,...userPlaces])
+    // Update can be done at before changing state but then it can then keep the app in loading state which may make users think that the app is stuck
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -35,9 +60,10 @@ function App() {
     });
 
     try{
-      await updateUserPlaces([selectedPlaces,...userPlaces])
+      await updateUserPlaces([selectedPlace,...userPlaces])
     } catch(error){
-      
+      setUserPlaces(userPlaces)
+      setErrorUpdatingPlaces({message: error.message || "Failed to update Places."})
     }
     
   }
@@ -47,11 +73,32 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
 
+    try{
+      await updateUserPlaces(
+        userPlaces.filter(place=>place.id!==selectedPlace.current.id)
+      )
+    } catch(error){
+      setUserPlaces(userPlaces)
+      setErrorUpdatingPlaces({
+        message: error.message || "Failed to delete Place"
+      })
+    }
+    
+
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
+
+  function handleError(){
+    setErrorUpdatingPlaces(null)
+  }
 
   return (
     <>
+    <Modal open={errorUpdatingPlaces} onClose={handleError}>
+      {errorUpdatingPlaces && (<Error 
+      title= "An Error occured" 
+      message={errorUpdatingPlaces.message}/>)}
+    </Modal>
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
@@ -68,12 +115,17 @@ function App() {
         </p>
       </header>
       <main>
+        {error && <Error title="An Error occured!!" message={error.message}/>}
+        {!error && 
         <Places
           title="I'd like to visit ..."
           fallbackText="Select the places you would like to visit below."
+          loadtingText={isFetching}
+          isLoading="Fetching Your Places ..."
           places={userPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
+        }
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
